57c86b9e267623f246ea5155e83f4d1e0d9e38b2
Jessie Shockey <shockeyjessie92@gmail.com>
2024-08-11 20:22:56 -0600

    Add Layouts

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Login(){
+    const onSubmit = (event)=>{
+        event.preventDefault();
+
+    }
 
     return(
-        <>
-            Login
-        </>
+        <form onSubmit={onSubmit}>
+            <h1 className="title">
+                Login into your account
+            </h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button className="btn btn-block">Login</button>
+            <p className="message">
+                Not Registered? <Link to='/signup'>Create an account</Link>
+            </p>
+        </form>
     )
 }
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,21 @@
 export default function Signup(){
+    const onSubmit = (event)=>{
+        event.preventDefault();
+    }
 
     return(
-        <>
-            Signup
-        </>
+        <form onSubmit={onSubmit}>
+            <h1 className="title">
+                Sign up for free
+            </h1>
+            <input type="email" placeholder="Full Name" />
+            <input type="email" placeholder="Email Address" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password Confirmation" />
+            <button className="btn btn-block">Signup</button>
+            <p className="message">
+                Already Registered? <Link to='/login'>Sign in</Link>
+            </p>
+        </form>
     )
 }
